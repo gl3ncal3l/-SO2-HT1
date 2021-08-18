413a2c46ac4fd9be9bdd32e3ba7159132404b8bf
gl3ncal3l <gl3ncal3l@gmail.com>
2021-08-17 23:10:25 -0600

    mod0

---
+++ mod0/mod0.c
@@ -0,0 +1,61 @@
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/mm.h>
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Grupo 6");
+MODULE_DESCRIPTION("Basic module");
+MODULE_VERSION("1.0");
+
+static int writeFile(struct seq_file* archivo, void ){
+    struct sysinfo inf;
+
+    long memoria_total;
+    long memoria_libre;
+    long memoria_buffer;
+    long memoria_compartida;
+    long memoria_freehigh;
+    long memoria_uso;
+
+    si_meminfo(&inf);
+
+    memoria_total = (inf.totalram * 4);
+    memoria_libre = (inf.freeram * 4);
+    memoria_buffer = (inf.bufferram * 4);
+    memoria_compartida = (inf.sharedram * 4);
+    memoria_freehigh = (inf.freehigh * 4);
+    memoria_uso = memoria_total - (memoria_libre + memoria_compartida + memoria_buffer * 2);
+
+    seq_printf(archivo, "Memoria total: %lu MB\n", memoria_total/1024);
+    seq_printf(archivo, "Memoria libre: %lu MB\n", memoria_libre/1024);
+    seq_printf(archivo, "Memoria consumida: %lu MB\n", memoria_uso/1024);
+    seq_printf(archivo, "Memoria en uso: %lu MB\n", (memoria_uso * 100)/memoria_total);
+    
+    return 0;
+}
+
+static int atOpen(struct inode* inode, struct file* file){
+    return single_open(file, writeFile, NULL);
+}
+
+static struct file_operations ops = {
+    .open = atOpen,
+    .read = seq_read
+};
+
+static int load_module(void){   
+    proc_create("mod0", 0, NULL, &ops);
+    printk(KERN_INFO "Hola Grupo 6\n");
+    return 0;
+}
+
+static void unload_module(void){
+    remove_proc_entry("mod0", NULL);
+    printk(KERN_INFO "Adiós Grupo 6\n");
+}
+
+module_init(load_module);
+module_exit(unload_module);
